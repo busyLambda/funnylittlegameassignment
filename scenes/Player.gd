ba6ee8a158f69f77f0aadf3bea7a0fc98406b83d
Susa Milán <mailtosnek@protonmail.com>
2023-11-13 19:29:12 +0100

    Charge jump but no real force or time inc

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -6,6 +6,7 @@ var jump_speed = -400.0
 # Get the gravity from the project settings so you can sync with rigid body nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var total = 0
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -13,7 +14,13 @@ func _physics_process(delta):
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		# This aint exactly the best since it don't workey so find a way to mesure the time :3
+		total += delta
+		
+	if Input.is_action_just_released("jump") and is_on_floor():
 		velocity.y = jump_speed
+		total = 0
+		
 
 	# Get the input direction.
 	var direction = Input.get_axis("move_left", "move_right")
