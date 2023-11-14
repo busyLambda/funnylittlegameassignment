ebf4d6071651443a33c2b50589765ec788f9c53b
Susa Milán <mailtosnek@protonmail.com>
2023-11-14 15:26:06 +0100

    Made running better + falling

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -44,3 +44,5 @@ func handle_jump():
 			jump_boost = limit
 		print(jump_boost)
 		velocity.y = jump_speed + jump_boost
+	if velocity.y >= 0 and !is_on_floor():
+		_animation_player.play("falling")
