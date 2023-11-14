6e108d165d04b571bbbf752ca94d4ac35dcd9fc9
Berényi Bence <berenyibence98@gmail.com>
2023-11-14 14:52:49 +0100

    message

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -4,7 +4,8 @@ var speed = 300.0
 var jump_speed = -400.0
 var max_charge = 30000
 
-# Get the gravity from the project settings so you can sync with rigid body nodes.
+@onready var _animation_player = $AnimatedSprite2D
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var total = 0
@@ -12,12 +13,30 @@ var limit = -140
 var pt = 0
 
 func _physics_process(delta):
-	# Add the gravity.
 	velocity.y += gravity * delta
 
-	# Handle Jump.
+	handle_movement()
+	handle_jump()
+
+	move_and_slide()
+
+func handle_movement():
+	var direction = 0
+	if Input.is_action_pressed("move_left"):
+		direction -= 1
+		_animation_player.play("run")
+		_animation_player.flip_h = true  # Invert the sprite when moving left
+	elif Input.is_action_pressed("move_right"):
+		direction += 1
+		_animation_player.play("run")
+		_animation_player.flip_h = false  # Do not invert the sprite when moving right
+	else:
+		_animation_player.play("idle")
+
+	velocity.x = direction * speed
+
+func handle_jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		# This aint exactly the best since it don't workey so find a way to mesure the time :3
 		pt = Time.get_unix_time_from_system()
 	if Input.is_action_just_released("jump") and is_on_floor():
 		var jump_boost = (pt - Time.get_unix_time_from_system()) * 100
@@ -25,9 +44,3 @@ func _physics_process(delta):
 			jump_boost = limit
 		print(jump_boost)
 		velocity.y = jump_speed + jump_boost
-
-	# Get the input direction.
-	var direction = Input.get_axis("move_left", "move_right")
-	velocity.x = direction * speed
-
-	move_and_slide()
