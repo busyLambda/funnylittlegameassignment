56cb1d330d60f1c09c3fc163a6a3d7b44b2336e8
Susa Milán <mailtosnek@protonmail.com>
2023-11-19 16:33:10 +0100

    Attack animation now works

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -25,23 +25,25 @@ func _physics_process(delta):
 	move_and_slide()
 
 func handle_attacking():
-	if is_attacking == false && Input.is_action_pressed("attack"):
+	if Input.is_action_pressed("attack"):
 		is_attacking = true
-		_animation_player.play("attack")
-		is_attacking = false
-
+		_animation_player.play("attack")	
+		
 func handle_movement():
 	var direction = 0
 	if Input.is_action_pressed("move_left"):
 		direction -= 1
-		_animation_player.play("run")
+		if !is_attacking:
+			_animation_player.play("run")
 		_animation_player.flip_h = true  # Invert the sprite when moving left
 	elif Input.is_action_pressed("move_right"):
 		direction += 1
-		_animation_player.play("run")
+		if !is_attacking:
+			_animation_player.play("run")
 		_animation_player.flip_h = false  # Do not invert the sprite when moving right
 	else:
-		_animation_player.play("idle")
+		if !is_attacking:
+			_animation_player.play("idle")
 
 	velocity.x = direction * speed
 
@@ -54,7 +56,13 @@ func handle_jump():
 			jump_boost = limit
 		print(jump_boost)
 		velocity.y = jump_speed + jump_boost
-	if velocity.y <= 0:
+		print(_animation_player.animation)
+	if velocity.y <= 0 and !is_attacking:
 		_animation_player.play("jump")
-	if velocity.y >= 0 and !is_on_floor():
+	if velocity.y >= 0 and !is_on_floor() and !is_attacking:
 		_animation_player.play("falling")
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if _animation_player.animation == "attack":
+		is_attacking = false
